9a6db289ab35e9c601bf34e6b384954e2d513d34
Kushal Pisavadia <kushi.p@gmail.com>
2013-03-11 19:24:18 +0000

    Add the api-path

--- src/gocardless/account.clj
+++ src/gocardless/account.clj
@@ -4,6 +4,8 @@
 
 (defonce ^:dynamic *gocardless-url* "https://gocardless.com")
 
+(defonce api-path "/api/v1")
+
 (defmacro with-account
   "Binds the specified Go Cardless account to the gocardless-account variable and executes the body."
   [account & body]
