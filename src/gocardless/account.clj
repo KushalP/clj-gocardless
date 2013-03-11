fda7c4650ca582bb399b32ebda348eb17f6ffeff
Kushal Pisavadia <kushi.p@gmail.com>
2013-03-11 19:36:28 +0000

    Add a way of binding to specific merchant id's

--- src/gocardless/account.clj
+++ src/gocardless/account.clj
@@ -2,6 +2,8 @@
 
 (defonce ^:dynamic *gocardless-account* nil)
 
+(defonce ^:dynamic *gocardless-merchant* nil)
+
 (defonce ^:dynamic *gocardless-url* "https://gocardless.com")
 
 (defonce api-path "/api/v1")
@@ -10,3 +12,8 @@
   "Binds the specified Go Cardless account to the gocardless-account variable and executes the body."
   [account & body]
   `(binding [*gocardless-account* ~account] ~@body))
+
+(defmacro with-merchant
+  "Binds the specified Go Cardless merchant id to the gocardless-merchant variable and executes the body."
+  [id & body]
+  `(binding [*gocardless-merchant* ~id] ~@body))
