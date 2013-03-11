7b836130b5e4a367d8c744b856835be7d632d42d
Kushal Pisavadia <kushi.p@gmail.com>
2013-03-11 19:02:49 +0000

    We'll be hitting a URL with our requests
    For the time being this is dynamic as we might provide a wrapper macro
    to convert any calls to use the sanbox equivalent.

--- src/gocardless/account.clj
+++ src/gocardless/account.clj
@@ -2,6 +2,8 @@
 
 (defonce ^:dynamic *gocardless-account* nil)
 
+(defonce ^:dynamic *gocardless-url* "https://gocardless.com")
+
 (defmacro with-account
   "Binds the specified Go Cardless account to the gocardless-account variable and executes the body."
   [account & body]
