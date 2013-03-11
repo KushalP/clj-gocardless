05819e4ff57fd17ff3885a0970c915e619e9308e
Kushal Pisavadia <kushi.p@gmail.com>
2013-03-11 23:21:50 +0000

    Provide a useful URL and description

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject gocardless "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+  :description "Clojure bindings for Go Cardless"
+  :url "https://github.com/KushalP/gocardless-clj"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.5.1"]
