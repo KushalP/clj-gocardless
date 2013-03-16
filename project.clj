0c076edb1bcdd98678288de27349ebed49a34ee8
Kushal Pisavadia <kushi.p@gmail.com>
2013-03-16 16:14:28 +0000

    Add cheshire as a project dependency
    We'll need to parse JSON as that's what the API uses.

--- project.clj
+++ project.clj
@@ -4,6 +4,7 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.5.1"]
-                 [clj-http "0.6.5"]]
+                 [clj-http "0.6.5"]
+                 [cheshire "5.0.2"]]
   :profiles {:dev {:dependencies [[betamax "0.1.3"]]}}
   :test-selectors {:default (constantly true)})
